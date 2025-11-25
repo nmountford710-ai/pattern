7c5c6ca6f56269869115e5ee244ee404871ebad5
nmountford710-ai <nmountford710@gmail.com>
2025-11-25 15:03:38 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const {
 
 const app = express();
 
-const { PORT } = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
